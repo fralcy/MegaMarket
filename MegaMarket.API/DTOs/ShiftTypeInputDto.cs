f5b448929906d9d4a3e636e8e64f3f62581eb29a
Quang Thinh Le <quangthinh2924@gmail.com>
2025-11-10 15:14:50 +0700

    feat: Add DTOs for User, ShiftType and Attendance management

---
+++ MegaMarket.API/DTOs/ShiftTypeInputDto.cs
@@ -0,0 +1,9 @@
+﻿namespace MegaMarket.API.DTOs;
+
+public class ShiftTypeInputDto
+{
+    public string Name { get; set; } = string.Empty;
+    public TimeSpan StartTime { get; set; }
+    public TimeSpan EndTime { get; set; }
+    public decimal WagePerHour { get; set; }
+}
